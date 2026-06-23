1801508a4599373b641800b10c8f6268c5a6f60d
Cleiton Luiz <SEU_EMAIL_DO_cleitonluizferreirap@gmail.com>
2026-06-22 22:55:31 -0400

    Melhora tela de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,35 +2,56 @@
 
 @section('conteudo')
 
-<div class="container">
+<div class="row justify-content-center">
+    <div class="col-md-5">
 
-    <h2>Login</h2>
+        <div class="card shadow-sm border-0">
+            <div class="card-body p-4">
 
-    @if($errors->any())
-        <div>
-            {{ $errors->first() }}
+                <h2 class="text-center mb-3">Login</h2>
+
+                <p class="text-center text-muted mb-4">
+                    Acesse o Registro Urbano
+                </p>
+
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        {{ $errors->first() }}
+                    </div>
+                @endif
+
+                <form action="/login" method="POST">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label class="form-label">E-mail</label>
+                        <input
+                            type="email"
+                            name="email"
+                            class="form-control"
+                            placeholder="Digite seu e-mail"
+                            required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Senha</label>
+                        <input
+                            type="password"
+                            name="password"
+                            class="form-control"
+                            placeholder="Digite sua senha"
+                            required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary w-100">
+                        Entrar
+                    </button>
+                </form>
+
+            </div>
         </div>
-    @endif
 
-    <form action="/login" method="POST">
-        @csrf
-
-        <div>
-            <label>E-mail</label>
-            <input type="email" name="email" required>
-        </div>
-
-        <div>
-            <label>Senha</label>
-            <input type="password" name="password" required>
-        </div>
-
-        <button type="submit">
-            Entrar
-        </button>
-
-    </form>
-
+    </div>
 </div>
 
 @endsection
